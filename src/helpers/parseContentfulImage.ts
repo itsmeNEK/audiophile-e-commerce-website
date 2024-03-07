d0153fa2a89b12ee8bd26a91f5de111311fe5c69
itsmeNEK <gasparkenken55@gmail.com>
2024-03-07 19:49:42 +0800

    refactor ParseContentfulImage

--- src/helpers/parseContentfulImage.ts
+++ src/helpers/parseContentfulImage.ts
@@ -1,9 +1,9 @@
 import { ThumbnailType } from '@/types/common/image'
 
 export function parseContentfulImage(data: ThumbnailType) {
-  const { secure_url, public_id, context, width, height } = data
+  const { secure_url, public_id, context } = data
   const imageUrl = secure_url
   const imagePublicId = public_id
   const altText = context.custom.alt
-  return { altText, imageUrl, imagePublicId, width, height }
+  return { altText, imageUrl, imagePublicId }
 }
